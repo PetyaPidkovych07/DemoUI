bb7b2a6fc05090a08d3de3ce91a3a9ac26f078a4
Your Name <you@example.com>
2024-04-03 13:38:55 +0300

    up

--- pages/recruitment_candidates_page.py
+++ pages/recruitment_candidates_page.py
@@ -138,9 +138,28 @@ class Recruitment_Candidate(BasePage):
     def click_on_icon_btn(self):
         self.wait.until(EC.element_to_be_clickable(self.ICON_BUTTON)).click()
 
+    def click_on_sort_icon_btn(self):
+        self.wait.until(EC.element_to_be_clickable(self.SORT_ICON_VACANCY)).click()
+
     def click_on_search_btn(self):
         self.wait.until(EC.element_to_be_clickable(self.SEARCH_BTN)).click()
         time.sleep(2)
+
+    def click_on_status_dropdown_btn(self):
+        self.wait.until(EC.element_to_be_clickable(self.STATUS_DROPDOWN)).click()
+
+    def click_on_sort_icon_candidate(self):
+        self.wait.until(EC.element_to_be_clickable(self.SORT_ICON_CANDIDATE)).click()
+
+    def click_on_sort_icon_hiring(self):
+        self.wait.until(EC.element_to_be_clickable(self.SORT_ICON_HIRING)).click()
+
+    def click_on_icon_from_calendar(self):
+        self.wait.until(EC.element_to_be_clickable(self.ICON_FROM_CALENDAR)).click()
+
+    def choose_rejected_from_dropdown(self):
+        self.wait.until(EC.element_to_be_clickable(self.ITEM_REJECTED_FROM_DROPDOWN)).click()
+
 
     def choose_qa_lead_from_dropdown(self):
         self.wait.until(EC.element_to_be_clickable(self.ITEM_QA_LEAD2_FROM_DROPDOWN)).click()
@@ -151,6 +170,9 @@ class Recruitment_Candidate(BasePage):
 
     def choose_it_manager_from_dropdown(self):
         self.wait.until(EC.element_to_be_clickable(self.ITEM_QA_LEAD5_FROM_DROPDOWN)).click()
+
+    def choose_software_engineer_from_dropdown(self):
+        self.wait.until(EC.element_to_be_clickable(self.ITEM_QA_LEAD3_FROM_DROPDOWN)).click()
 
 
 
@@ -230,6 +252,446 @@ class Recruitment_Candidate(BasePage):
                    'Associate IT Manager']
         print(vacancy)
         assert vacancy == size_list
+
+
+    def are_fonded_software_enginner_when_chosen_from_dropdown(self):
+        vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.VACANCY_COLUMN))
+        size_list = []
+        for size in vacancy_column:
+            size_list.append(size.text)
+        print(size_list)
+
+        vacancy = ['Vacancy', 'Software Engineer', 'Software Engineer', 'Software Engineer']
+        print(vacancy)
+        assert vacancy == size_list
+
+    def are_displayed_rejected_when_chosen_from_dropdown(self):
+        vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.STATUS_COLUMN))
+        size_list = []
+        for size in vacancy_column:
+            size_list.append(size.text)
+        print(size_list)
+
+        vacancy = ['Status', 'Rejected', 'Rejected', 'Rejected', 'Rejected', 'Rejected']
+        print(vacancy)
+        assert vacancy == size_list
+
+    def are_sorted_ascending_vacancy_in_table(self):
+        action = ActionChains(self.driver)
+        element = self.wait.until(EC.element_to_be_clickable(self.SORT_CHOOSE_ITEM_VACANCY_AS))
+        action.move_to_element(element).click().perform()
+        vacancy = []
+
+        try:
+
+            for i in range(1, 6):
+                vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.All_USERS))
+                for user in vacancy_column:
+                    vacancy.append(user.find_element("xpath",".//div[contains(@class, 'oxd-table-cell oxd-padding-cell')][2]").text)
+
+                xpath_text = "(//nav[@role='navigation']//ul[@class='oxd-pagination__ul']//li[" + str(i) + "])"
+                time.sleep(2)
+                self.driver.find_element("xpath", xpath_text).click()
+        except NoSuchElementException:
+            pass
+
+        y = 'Associate IT Manager', 'Senior QA Lead', 'Software Engineer' in vacancy
+        print(y)
+
+        print(f"Users: {vacancy}")
+
+    def are_sorted_descending_vacancy_in_table(self):
+        action = ActionChains(self.driver)
+        element = self.wait.until(EC.element_to_be_clickable(self.SORT_CHOOSE_ITEM_VACANCY_DES))
+        action.move_to_element(element).click().perform()
+        vacancy = []
+
+        try:
+
+            for i in range(1, 6):
+                vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.All_USERS))
+                for user in vacancy_column:
+                    vacancy.append(user.find_element("xpath",
+                                                     ".//div[contains(@class, 'oxd-table-cell oxd-padding-cell')][2]").text)
+
+                xpath_text = "(//nav[@role='navigation']//ul[@class='oxd-pagination__ul']//li[" + str(i) + "])"
+                time.sleep(2)
+                self.driver.find_element("xpath", xpath_text).click()
+        except NoSuchElementException:
+            pass
+
+        y = 'Software Engineer', 'Senior QA Lead', 'Associate IT Manager' in vacancy
+        print(y)
+
+        print(f"Users: {vacancy}")
+
+
+
+    def are_sorted_ascending_candidate_in_table(self):
+        action = ActionChains(self.driver)
+        element = self.wait.until(EC.element_to_be_clickable(self.SORT_CHOOSE_ITEM_CANDIDATE_AS))
+        action.move_to_element(element).click().perform()
+        candidate = []
+
+        try:
+
+            for i in range(1, 6):
+                vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.All_USERS))
+                for user in vacancy_column:
+                    candidate.append(user.find_element("xpath",
+                                                     ".//div[contains(@class, 'oxd-table-cell oxd-padding-cell')][3]").text)
+
+                xpath_text = "(//nav[@role='navigation']//ul[@class='oxd-pagination__ul']//li[" + str(i) + "])"
+                time.sleep(2)
+                self.driver.find_element("xpath", xpath_text).click()
+        except NoSuchElementException:
+            pass
+
+        y = 'Jennifer Clinton', 'Chris Harris', 'Peter Smith' in candidate
+        print(y)
+
+        print(f"Users: {candidate}")
+
+
+
+    def are_sorted_descending_candidate_in_table(self):
+        action = ActionChains(self.driver)
+        element = self.wait.until(EC.element_to_be_clickable(self.SORT_CHOOSE_ITEM_CANDIDATE_DES))
+        action.move_to_element(element).click().perform()
+        candidate = []
+
+        try:
+
+            for i in range(1, 6):
+                vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.All_USERS))
+                for user in vacancy_column:
+                    candidate.append(user.find_element("xpath",
+                                                     ".//div[contains(@class, 'oxd-table-cell oxd-padding-cell')][3]").text)
+
+                xpath_text = "(//nav[@role='navigation']//ul[@class='oxd-pagination__ul']//li[" + str(i) + "])"
+                time.sleep(2)
+                self.driver.find_element("xpath", xpath_text).click()
+        except NoSuchElementException:
+            pass
+
+        y = 'Martine Tennat', 'Peter Smith', 'Jennifer Clinton' in candidate
+        print(y)
+
+        print(f"Users: {candidate}")
+
+    def are_sorted_ascending_hiring_in_table(self):
+        action = ActionChains(self.driver)
+        element = self.wait.until(EC.element_to_be_clickable(self.SORT_CHOOSE_ITEM_HIRING_AS))
+        action.move_to_element(element).click().perform()
+        hiring = []
+
+        try:
+
+            for i in range(1, 6):
+                vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.All_USERS))
+                for user in vacancy_column:
+                    hiring.append(user.find_element("xpath",
+                                                       ".//div[contains(@class, 'oxd-table-cell oxd-padding-cell')][4]").text)
+
+                xpath_text = "(//nav[@role='navigation']//ul[@class='oxd-pagination__ul']//li[" + str(i) + "])"
+                time.sleep(2)
+                self.driver.find_element("xpath", xpath_text).click()
+        except NoSuchElementException:
+            pass
+
+        y = 'Odis Adalwin', 'Linda Jane Anderson', 'Dominic Chase' in hiring
+        print(y)
+        print(f"Users: {hiring}")
+
+
+    def are_sorted_descending_hiring_in_table(self):
+        action = ActionChains(self.driver)
+        element = self.wait.until(EC.element_to_be_clickable(self.SORT_CHOOSE_ITEM_HIRING_DES))
+        action.move_to_element(element).click().perform()
+        hiring = []
+
+        try:
+
+            for i in range(1, 6):
+                vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.All_USERS))
+                for user in vacancy_column:
+                    hiring.append(user.find_element("xpath",
+                                                       ".//div[contains(@class, 'oxd-table-cell oxd-padding-cell')][4]").text)
+
+                xpath_text = "(//nav[@role='navigation']//ul[@class='oxd-pagination__ul']//li[" + str(i) + "])"
+                time.sleep(2)
+                self.driver.find_element("xpath", xpath_text).click()
+        except NoSuchElementException:
+            pass
+        print(f"Users: {hiring}")
+
+
+
+    def are_fonded_valid_date_via_calendar(self, year="2022", month="July", day="15"):
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        time.sleep(3)
+
+        self.wait.until(EC.element_to_be_clickable(self.ICON_TO_CALENDAR)).click()
+
+        year = "2022"
+        month = "July"
+        day = "28"
+
+        time.sleep(5)
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        self.wait.until(EC.element_to_be_clickable(self.SEARCH_BTN)).click()
+        time.sleep(2)
+        vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.VACANCY_COLUMN))
+        size_list = []
+        for size in vacancy_column:
+            size_list.append(size.text)
+
+        y = ['Vacancy', 'Sales Representative']
+        print(y)
+        assert y == size_list
+
+
+
+    def are_fonded_july_2022_via_calendar(self, year="2022", month="July", day="12"):
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        time.sleep(3)
+
+        self.wait.until(EC.element_to_be_clickable(self.ICON_TO_CALENDAR)).click()
+
+        year = "2022"
+        month = "July"
+        day = "14"
+
+        time.sleep(5)
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        self.wait.until(EC.element_to_be_clickable(self.SEARCH_BTN)).click()
+        time.sleep(2)
+        vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.VACANCY_COLUMN))
+        size_list = []
+        for size in vacancy_column:
+            size_list.append(size.text)
+
+        y = ['Vacancy', 'Software Engineer']
+        print(y)
+        assert y == size_list
+
+
+    def are_fonded_july_11_via_calendar(self, year = "2022", month = "July", day = "8"):
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        time.sleep(3)
+
+        self.wait.until(EC.element_to_be_clickable(self.ICON_TO_CALENDAR)).click()
+
+        year = "2022"
+        month = "July"
+        day = "11"
+
+        time.sleep(5)
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        self.wait.until(EC.element_to_be_clickable(self.SEARCH_BTN)).click()
+        time.sleep(2)
+        vacancy_column = self.wait.until(EC.presence_of_all_elements_located(self.VACANCY_COLUMN))
+        size_list = []
+        for size in vacancy_column:
+            size_list.append(size.text)
+
+        vacancy = ['Vacancy', 'Senior Support Specialist', 'Senior QA Lead', 'Sales Representative']
+        print(vacancy)
+        assert vacancy == size_list
+
+
+
+    def are_fonded_invalid_data_via_calendar(self, year = "2018", month = "July", day = "9"):
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        time.sleep(3)
+
+        self.wait.until(EC.element_to_be_clickable(self.ICON_TO_CALENDAR)).click()
+
+        year = "2019"
+        month = "July"
+        day = "11"
+
+        time.sleep(5)
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        self.wait.until(EC.element_to_be_clickable(self.SEARCH_BTN)).click()
+        time.sleep(2)
+        blur = self.wait.until(EC.presence_of_element_located(self.PUSH_NOTIFICATION)).text
+        assert blur == 'No Records Found'
+
+
+
+
+    def are_searched_for_invalid_data_via_calendar(self, year="2018", month="July", day ="9"):
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        time.sleep(3)
+
+        self.wait.until(EC.element_to_be_clickable(self.ICON_TO_CALENDAR)).click()
+
+        year = "2019"
+        month = "July"
+        day = "12"
+
+        time.sleep(5)
+        while True:
+            yr = self.driver.find_element(*self.CURRENT_YEAR).text
+            mon = self.driver.find_element(*self.CURRENT_MONTH).text
+
+            if yr == year and mon == month:
+                break;
+            else:
+                self.driver.find_element(*self.PREVIOUS_BTN_IN_CALENDAR).click()
+        time.sleep(3)
+
+        dates = self.driver.find_elements(*self.CHOOSE_DATE)
+        time.sleep(3)
+        for ele in dates:
+            if ele.text == day:
+                ele.click()
+                break
+        self.wait.until(EC.element_to_be_clickable(self.SEARCH_BTN)).click()
+        time.sleep(2)
+        blur = self.wait.until(EC.presence_of_element_located(self.PUSH_NOTIFICATION)).text
+        assert blur == 'No Records Found'
 
 
 
