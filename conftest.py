6e4d69f523cdcc9cd4f52bbb9c5df38617b230d6
Nedjy <84965240+PetyaPidkovych07@users.noreply.github.com>
2026-01-27 21:43:56 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -23,8 +23,6 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920, 1080")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     request.cls.driver = driver
