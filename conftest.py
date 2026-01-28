eaa46d02f376b5d936a646cd4e34723fcd2ebc6e
Nedjy <84965240+PetyaPidkovych07@users.noreply.github.com>
2026-01-28 11:32:04 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -26,6 +26,8 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920, 1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     request.cls.driver = driver
