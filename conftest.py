40a596bc1bce1591b68bf6e21be7cce950437f68
Your Name <you@example.com>
2026-01-30 14:32:28 +0200

    Again fix on CI

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
@@ -14,10 +11,38 @@ def driver(request):
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # Використовує системний chromedriver, який ми ставимо через apt-get
+    driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
     driver.quit()
+
+
+
+
+
+
+
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+#
+# @pytest.fixture(scope="function", autouse=True)
+# def driver(request):
+#     options = Options()
+#     #options.add_argument("--headless")
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--disable-dev-shm-usage")
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--window-size=1920,1080")
+#
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#     request.cls.driver = driver
+#     yield driver
+#     driver.quit()
 
 
 # from webbrowser import Chrome
