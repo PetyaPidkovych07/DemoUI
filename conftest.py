44b363916229139fab97e7e9f3498e50a01b192b
Your Name <you@example.com>
2026-01-30 15:07:21 +0200

    previus CI

--- conftest.py
+++ conftest.py
@@ -1,44 +1,10 @@
-import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope="function", autouse=True)
-def driver(request):
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
 
-    if os.getenv("CI"):
-        # Ми у GitHub Actions → використовуємо системний Chrome
-        options.binary_location = "/usr/bin/google-chrome"
-        driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
-    else:
-        # Локально → webdriver_manager сам качає драйвер
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    request.cls.driver = driver
-    yield driver
-    driver.quit()
-
-
-
-
-
-
-
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-#
 # @pytest.fixture(scope="function", autouse=True)
 # def driver(request):
 #     options = Options()
@@ -54,42 +20,40 @@ def driver(request):
 #     driver.quit()
 
 
-# from webbrowser import Chrome
-#
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-#
-#
-#
-#
+from webbrowser import Chrome
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+
+
 # @pytest.fixture()
 # def driver(request):
 #     driver = Chrome(ChromeDriverManager().install())
 #     request.cls.driver = driver
 #     yield driver
-#
-#
-#
-#
-# @pytest.fixture(scope="function", autouse=True)
-# def driver(request):
-#     options = Options()
-#     #options.add_argument("--headless")  # важливо для сучасного Chrome
-#     options.add_argument("--no-sandbox")
-#     #options.add_argument("--disable-dev-shm-usage")
-#     options.add_argument("--window-size=1920, 1080")
-#     # options.add_argument("--disable-gpu")
-#     # options.add_argument("--disable-software-rasterizer")
-#     driver = webdriver.Chrome(options=options)
-#     driver.maximize_window()
-#     request.cls.driver = driver
-#     yield driver
-#     driver.quit()
-#
-#
-#
-#
+
+
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    options = Options()
+    options.add_argument("--headless")  # важливо для сучасного Chrome
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920, 1080")
+    options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-software-rasterizer")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield driver
+    driver.quit()
+
+
+
+
