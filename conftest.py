25f028429708e358835a0be3acd14b901f135fda
Your Name <you@example.com>
2026-01-30 14:52:01 +0200

    Final Fix with CI

--- conftest.py
+++ conftest.py
@@ -1,18 +1,27 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
     options = Options()
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    # Використовує системний chromedriver, який ми ставимо через apt-get
-    driver = webdriver.Chrome(options=options)
+    if os.getenv("CI"):
+        # Ми у GitHub Actions → використовуємо системний Chrome
+        options.binary_location = "/usr/bin/google-chrome"
+        driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
+    else:
+        # Локально → webdriver_manager сам качає драйвер
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
     request.cls.driver = driver
     yield driver
     driver.quit()
