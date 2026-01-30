5ff39d531a944cbbbfdd4da501db2177b724fd54
Your Name <you@example.com>
2026-01-30 16:04:04 +0200

    Yra

--- conftest.py
+++ conftest.py
@@ -29,6 +29,28 @@ from selenium.webdriver.common.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    options = Options()
+    options.add_argument("--headless")  # важливо для сучасного Chrome
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920, 1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    driver = webdriver.Chrome(options=options)
+    #driver.maximize_window()
+    request.cls.driver = driver
+    yield driver
+    driver.quit()
+
+
+
+
+
+
+
+
 
 
 
@@ -39,20 +61,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 #     yield driver
 
 
-@pytest.fixture(scope="function", autouse=True)
-def driver(request):
-    options = Options()
-    options.add_argument("--headless")  # важливо для сучасного Chrome
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920, 1080")
-    options.add_argument("--disable-gpu")
-    # options.add_argument("--disable-software-rasterizer")
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
-    request.cls.driver = driver
-    yield driver
-    driver.quit()
 
 
 
