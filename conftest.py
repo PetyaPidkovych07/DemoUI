3182fe15bc9a03b3a13057a558439505d9a44c1c
Your Name <you@example.com>
2026-01-30 14:25:17 +0200

    Fix CI

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
